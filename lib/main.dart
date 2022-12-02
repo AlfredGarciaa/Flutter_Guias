b34fb8cb2807e3977a967262aada185dbbeb0578
Alfred <alfred.garcia@ucb.edu.bo>
2022-12-02 10:14:26 -0400

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      Center( child: Text('hey this is flutter, the new way for developing funny and easy hybrid mobile app',
-              textDirection: TextDirection.ltr, ),)
+      const Center( child: Text('hey this is flutter, the new way for developing funny and easy hybrid mobile app',
+                    textDirection: TextDirection.ltr, ),)
   );
 }
